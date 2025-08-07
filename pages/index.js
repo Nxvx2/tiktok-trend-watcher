09dce760eb5061f7cdc694983104818ed1e0adf1
Niyi <test@Niyis-MacBook-Pro.local>
2025-08-07 16:42:16 +0100

    Trigger redeploy with working structure

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hello, TikTok Trend Watcher 👋</h1>
-      <p>This is your first deployed Next.js app!</p>
+      <h1>Hello world, TikTok trend watcher is online!</h1>
     </div>
   );
 }
